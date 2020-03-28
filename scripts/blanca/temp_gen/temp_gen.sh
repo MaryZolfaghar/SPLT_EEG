ed429708330dc565d2be50b59b0d9a46b49fa4f1
Mary <mazo2428@colorado.edu>
2020-03-28 15:58:11 -0400

    change blanca sh, save result folder from tempGen to temp_gen

--- scripts/blanca/temp_gen/temp_gen.sh
+++ scripts/blanca/temp_gen/temp_gen.sh
@@ -11,14 +11,14 @@ conda activate /pl/active/ccnlab/users/zolfaghar/EEGexp
 
 #=============================================================================#
 
-# selected_subj=( 1 2 3 4 5 7 8 9 10 12 15 16 17 18 19 20 21 22 23 24 25 26 27 28\
-                # 29 30 31 32 33 34 35 36 37 38 39 42 43 44 45 46 47 48 51 52 53 \
-                # 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74)
+selected_subj=( 1 2 3 4 5 7 8 9 10 12 15 16 17 18 19 20 21 22 23 24 25 26 27 28\
+                29 30 31 32 33 34 35 36 37 38 39 42 43 44 45 46 47 48 51 52 53 \
+                55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74)
 
 
 #selected_subj=( 1 2 3 4 5 7 8 9 10 12 15 16 17 18 19 20 21 )
 
-selected_subj=(1)
+#selected_subj=(1)
 
 start time
 #sleep 1h
@@ -40,7 +40,7 @@ then
 
     python temp_gen.py \
     --SAVE_EPOCH_ROOT ../../../../../../data/SPLT5.2/epochs/ \
-    --SAVE_RESULT_ROOT ../results/tempGen/ \
+    --SAVE_RESULT_ROOT ../results/temp_gen/ \
     --subj_num $sbj_num \
     --applyBaseline_bool \
     --cond_filter none \
