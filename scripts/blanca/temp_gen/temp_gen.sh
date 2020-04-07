8a981d754ca6ef8f3e49a892a1f9d9d85a67de83
Mary <mazo2428@colorado.edu>
2020-04-07 11:44:34 -0400

    debugged editted blanca scripts

--- scripts/blanca/temp_gen/temp_gen.sh
+++ scripts/blanca/temp_gen/temp_gen.sh
@@ -26,7 +26,7 @@ selected_subj=( 1 2 3 4 5 7 8 9 10 12 15 16 17 18 19 20 21 22 23 24 25 26 27 28\
                 29 30 31 32 33 34 35 36 37 38 39 42 43 44 45 46 47 48 51 52 53 \
                 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74)
 
-selected_subj=(1 2)
+selected_subj=(1)
 
 start time
 date +"%H:%M:%S"
@@ -42,10 +42,10 @@ python temp_gen.py \
 --SAVE_EPOCH_ROOT ../../../data/SPLT5.2/epochs/aft_ICA_rej/ \
 --SAVE_RESULT_ROOT ../results/temp_gen/ \
 --subj_num $sbj_num \
---applyBaseline_bool \
 --cond_filter none \
 --cond_block later \
---cond_time prestim \
+--cond_decoding removeevoked \
+--n_splits 100 \
 
 done
 
