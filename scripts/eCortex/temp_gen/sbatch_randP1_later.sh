27166df6e8c70fc5f47f133f39c04b9917ddb4e0
Mary <mazo2428@colorado.edu>
2020-05-03 11:21:50 -0400

    added scripts for eCortex

--- scripts/eCortex/temp_gen/sbatch_randP1_later.sh
+++ scripts/eCortex/temp_gen/sbatch_randP1_later.sh
@@ -13,6 +13,7 @@ conda activate /home/mazlfghr/.conda/envs/DeepLearningEEG
 selected_subj_P1=( 1 2 3 4 5 7 8 9 10 12 15 16 42 43 44 45 46 47 48 51 52 53 \
                    55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 71 72 73 74)
 # selected_subj_P2=( 18 19 20 21 23 24 26 28 29 30 31 32 33 34 35 36 38 39 )
+echo "Random - pattern 1 - Later Blocks"
 
 for i in "${selected_subj_P1[@]}"
 do
