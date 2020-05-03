694a67062d50af48b6386f341b79ec454b112324
Mary <mazo2428@colorado.edu>
2020-05-03 11:13:04 -0400

    added scripts for eCortex

--- scripts/eCortex/temp_gen/sbatch_randP1_early.sh
+++ scripts/eCortex/temp_gen/sbatch_randP1_early.sh
@@ -19,8 +19,10 @@ do
 echo $i
 sbj_num=$i
 fn_str="rand_avgP1_scores_timeGen_earlyBlocks_noneFilter_PrePost_decodremoveevoked_bslineFalse_3k_Trgt_Loc_prev_$sbj_num"
-# fn_str="rand_avgP1_scores_timeGen_laterBlocks_noneFilter_PrePost_decodremoveevoked_bslineFalse_3k_Trgt_Loc_prev_$sbj_num"
+echo $fn_str
+
 full_fn=/home/mazlfghr/projects/SPLT/results/temp_gen/eCortex/$fn_str
+echo $full_fn
 
 if [ ! -f $full_fn ]
 then
