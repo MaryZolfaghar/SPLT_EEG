0d138947fcac47a90d91f08394aa41287064fc8c
Mary <mazo2428@colorado.edu>
2020-05-05 02:18:35 -0400

    scripts eCortex

--- scripts/blanca/temp_gen/sbatch_b.sh
+++ scripts/blanca/temp_gen/sbatch_b.sh
@@ -1,20 +1,30 @@
-#!/usr/bin/env bash
-#SBATCH -p localLimited
-#SBATCH -A ecortex
+#!/bin/bash
+#SBATCH --time=120:00:00
+#SBATCH -c 16
+#SBATCH --qos=blanca-ccn
 #SBATCH --mem=10G
 
 export HOME=`getent passwd $USER | cut -d':' -f6`
+echo $HOME
 export PYTHONUNBUFFERED=1
+
 echo Running on $HOSTNAME
 
-source /usr/local/anaconda3/etc/profile.d/conda.sh
-conda activate /home/mazlfghr/.conda/envs/DeepLearningEEG
+source /pl/active/ccnlab/conda/etc/profile.d/conda.sh
+conda activate /pl/active/ccnlab/users/zolfaghar/conda/SPLTEEG
+
+# gpus=$(echo $CUDA_VISIBLE_DEVICES | tr "," "\n")
+# for gpu in $gpus
+# do
+# echo "Setting fan for" $gpu "to full"
+# nvidia_fancontrol full $gpu
+# done
+
+PYTHONPATH=/pl/active/ccnlab/users/zolfaghar/finalCodes_version5.2/github/SPLT_EEG/
+
+
 echo Running on $HOSTNAME
-selected_subj=( 1 2 3 4 5 7 8 9 10 12 15 16 18 19 20 21 23 24 26 28\
-                29 30 31 32 33 34 35 36 38 39 42 43 44 45 46 47 48 51 52 53\
-                55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 71 72 73 74 )
-echo Running on $HOSTNAME
-for i in "${selected_subj[@]}"
+for i in "${selected_subj_P1[@]}"
 do
 echo $i
 sbj_num=$i
@@ -33,8 +43,8 @@ then
      # - removed evoked - #
      # - 3k - #
      # - 100 iterations - #
-     sbatch scripts/eCortex/temp_gen/temp_gen_earlyPrePost_none_noBsline_rmvevoked.sh &
-     sbatch scripts/eCortex/temp_gen/temp_gen_laterPrePost_none_noBsline_rmvevoked.sh &
+     sbatch scripts/blanca/temp_gen/temp_gen_earlyPrePost_none_noBsline_rmvevoked.sh &
+     sbatch scripts/blanca/temp_gen/temp_gen_laterPrePost_none_noBsline_rmvevoked.sh &
 else
      echo "----------------------------"
      echo "File $sbj_num already exists."
