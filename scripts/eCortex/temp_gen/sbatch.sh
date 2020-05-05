e6b75014acc9bd0099800ffc121054b658ea5598
Mary <mazo2428@colorado.edu>
2020-05-05 02:20:34 -0400

    scripts eCortex

--- scripts/eCortex/temp_gen/sbatch.sh
+++ scripts/eCortex/temp_gen/sbatch.sh
@@ -13,6 +13,7 @@ echo Running on $HOSTNAME
 selected_subj=( 1 2 3 4 5 7 8 9 10 12 15 16 18 19 20 21 23 24 26 28\
                 29 30 31 32 33 34 35 36 38 39 42 43 44 45 46 47 48 51 52 53\
                 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 71 72 73 74 )
+
 echo Running on $HOSTNAME
 for i in "${selected_subj[@]}"
 do
